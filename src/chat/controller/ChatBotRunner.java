86c1b500f605c6faf1c25b18575ac854aec72679
alorgill <yui@Ghanoo>
2015-11-03 22:29:26 -0700

    Missed one.

--- src/chat/controller/ChatBotRunner.java
+++ src/chat/controller/ChatBotRunner.java
@@ -1,10 +1,9 @@
 package chat.controller;
-
 public class ChatBotRunner
 {
-	public static void main (String [] args)
-	{
-		ChatbotController myChatController = new ChatbotController();
-		myChatController.start();
-	}
+  public static void main (String [] args)
+  {
+    ChatbotController myChatController = new ChatbotController();
+    myChatController.start();
+  }
 }
