a8a14ca537ae7ad6d1b5d3da3789922110aa7acb
Tobin801 <tand6053@740s-pl-103.local>
2015-11-23 13:26:36 -0700

    Fixed it

--- src/chat/view/ChatbotPanel.java
+++ src/chat/view/ChatbotPanel.java
@@ -1,7 +1,9 @@
 package chat.view;
 
 import javax.swing.*;
+
 import java.awt.event.*;
+
 import chat.controller.ChatbotController;
 
 public class ChatbotPanel extends JPanel
@@ -10,4 +12,34 @@ public class ChatbotPanel extends JPanel
 	private JButton firstButton;
 	private JTextField firstTextField;private SpringLayout baseLayout;
 	private JTextArea firstTextArea;
+	
+	public ChatbotPanel(ChatbotController baseController)
+	{
+		this.baseController = baseController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
+		this.add(firstTextArea);
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 134, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -70, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 79, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 159, SpringLayout.WEST, this);
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
