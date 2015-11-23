6e74d89cfa6ff2937ee7a62a07216b080e200f6a
Tobin801 <tand6053@740s-pl-103.local>
2015-11-23 13:09:49 -0700

    Added guy

---
+++ src/chat/view/ChatbotPanel.java
@@ -0,0 +1,13 @@
+package chat.view;
+
+import javax.swing.*;
+import java.awt.event.*;
+import chat.controller.ChatbotController;
+
+public class ChatbotPanel extends JPanel
+{
+	private ChatbotController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;private SpringLayout baseLayout;
+	private JTextArea firstTextArea;
+}
